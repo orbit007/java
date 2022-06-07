dd763d8b6a941eaf9f56a1ef5a3c6931bea87871
orbit007 <68804524+orbit007@users.noreply.github.com>
2022-06-07 12:21:07 -0700

    java

--- Recursion/DisplayArray.java
+++ Recursion/DisplayArray.java
@@ -26,3 +26,34 @@ public class Main{
         System.out.println(arr[n-1]);
     }
 }
+
+
+
+import java.util.*;
+public class Main{
+    public static void main(String[] args)
+    {
+        Scanner scn= new Scanner(System.in);
+        int n=scn.nextInt();
+
+        int arr[]=new int[n];
+
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=scn.nextInt();
+        }
+
+        dis(0,arr);
+
+    }
+    public static void dis(int k,int arr[])
+    {
+        if(k==arr.length)
+        {
+            return;
+        }
+        System.out.println(arr[k]);
+        dis(k+1,arr);
+        
+    }
+}
