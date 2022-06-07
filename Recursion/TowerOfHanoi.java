da9b226d129c05b45f7b99dd19997dff375de25a
orbit007 <68804524+orbit007@users.noreply.github.com>
2022-06-07 09:13:57 -0700

    java

---
+++ Recursion/TowerOfHanoi.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args)
+    {
+        Scanner scn= new Scanner(System.in);
+        int n=scn.nextInt();
+        int src=scn.nextInt();
+        int dest=scn.nextInt();
+        int helper = scn.nextInt();
+        toh(n,src,dest,helper);
+
+    }
+
+    public static void toh(int n,int src,int dest,int helper)
+    {
+        if(n==0)
+        {
+            return;
+        }
+
+        toh(n-1,src,helper,dest);  // fath1   a
+        System.out.println(n+ "["+src+" -> "+dest+"]");
+        toh(n-1,helper,dest,src);  //fath2    b
+    }
+}
