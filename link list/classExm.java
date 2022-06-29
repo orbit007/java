6706826884686b65588241e835362c96bce67d80
orbit007 <68804524+orbit007@users.noreply.github.com>
2022-06-28 22:31:31 -0700

    Create classExm.java

---
+++ link list/classExm.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main{
+  public static class persion{
+    String name;
+    int age;
+  }
+  public static void main(String[] args)
+  {
+
+    persion p1=new persion();
+    p1.name="abhishek";
+    p1.age=24;
+    System.out.println(p1.name+" "+p1.age);
+
+  }
+}
+ 
