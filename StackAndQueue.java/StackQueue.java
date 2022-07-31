d92d98363c5fa76ad753b213282e46889259362e
orbit007 <68804524+orbit007@users.noreply.github.com>
2022-07-30 18:22:32 -0700

    Create StackQueue.java

---
+++ StackAndQueue.java/StackQueue.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.ArrayDeque;
+public class Main{
+
+  public static void QueueIntro()
+  {
+    Queue<Integer> queue=new ArrayDeque<>();
+    System.out.println(queue);
+    queue.add(10);
+    System.out.println(queue.size()+"  "+queue);
+    queue.add(20);
+    System.out.println(queue.size()+"  "+queue);
+    queue.add(30);
+    System.out.println(queue.size()+"  "+queue);
+    queue.add(40);
+    System.out.println(queue.size()+"  "+queue);
+    queue.add(50);
+    System.out.println(queue.size()+"  "+queue);
+
+    for (int ele: queue)
+    {
+      System.out.println(ele);
+    }
+
+    queue.remove();
+    System.out.println(queue);
+
+    System.out.println(queue.peek());
+  }
+  public static void main(String args[])
+  {
+    Stack<Integer> st=new Stack<Integer>();
+    System.out.println(st);
+    st.push(10);
+    System.out.println(st.size()+"  "+st);
+    st.push(20);
+    System.out.println(st.size()+"  "+st);
+    st.push(30);
+    System.out.println(st.size()+"  "+st);
+    st.push(40);
+    System.out.println(st.size()+"  "+st);
+
+    for (int ele: st)
+    {
+      System.out.println(ele);
+    }
+    System.out.println(st.pop());
+    System.out.println(st);
+    System.out.println(st.peek());
+
+    QueueIntro();
+  }
+}
