f8473b1773cad4c5dc0665ee6164e926d68ffb0b
orbit007 <68804524+orbit007@users.noreply.github.com>
2022-08-08 13:13:14 -0700

    Create Balanced Brackets.java

---
+++ StackAndQueue.java/Balanced Brackets.java
@@ -0,0 +1,54 @@
+import java.util.*;
+public class Main{
+   public static void main(String args[])
+   {
+      Scanner scn=new Scanner(System.in);
+      String s=scn.nextLine();
+      System.out.println(bal(s));
+
+   }
+
+   public static boolean bal(String s)
+   {
+      Stack<Character> st = new Stack<>();
+
+      for(int i=0;i<s.length();i++)
+      {
+         char ch=s.charAt(i);
+
+         if(ch=='(' || ch=='{' || ch=='[')
+         {
+            st.push(ch);
+
+         }
+         else if(ch==')')
+         {
+            if(st.size()==0 || st.peek() !='(')
+            {
+               return false;
+            }
+            st.pop();
+
+         }
+         else if(ch==']')
+         {
+            if(st.size()==0 || st.peek() !='[')
+            {
+               return false;
+            }
+            st.pop();
+            
+         }
+         else if(ch=='}')
+         {
+            if(st.size()==0 || st.peek() !='{')
+            {
+               return false;
+            }
+            st.pop();
+            
+         }
+      }
+      return st.size()==0;
+   }
+}
